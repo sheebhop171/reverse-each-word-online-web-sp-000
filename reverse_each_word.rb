efcb95aa9e60470b1de10651ccf43bce3576f177
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-22 18:56:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,8 @@
-sentence = "Hello there, and how are you today?"
-
 def reverse_each_word(sentence)
-
+  new_sentence = []
+  new_sentence = sentence.split(" ")
+  sentence.each do |word|
+  new_sentence << "#{word}"
+  end
+  new_sentence
 end
