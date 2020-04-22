505458a422abb64b4530f477717d3d2e46c0d63a
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-22 19:09:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
  def reverse_each_word(sentence)
   sentence1 = sentence.split(" ") #converts sentence from string to array
   sentence2 = [] #creates new array
-  sentence1.each do |word| #enumerates over each word in array
+  sentence1.collect do |word| #enumerates over each word in array
   sentence2.reverse << "#{word}" #reverse word & puts it into new array
   sentence3 = sentence2.join #reverts array back to string
   end
