9734a9b5dd6ca0fc364ed5524a39fb771d2b038a
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-22 19:22:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
- def reverse_each_word(string)
-   array = string.split
-   array.collect do |word|
+ def reverse_each_word(sentence)
+   sentence1 = string.split
+   sentence2 = []
+   sentence1.collect do |word|
      puts word.reverse
    end
  end
